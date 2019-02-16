4fc64d16613f6f4b2b03e0a86f34c1ab7ae1b990
ceb3745 <ceb3745@g.rit.edu>
2019-02-16 15:19:38 -0500

    Added in the functionality of checking user session data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import UserProfile from '../src/UserProfile.js';
+import Login from '../src/components/Login.js';
+import Home from '../src/components/Home.js';
+import '../src/App.css';
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if(UserProfile.getName() !== ""){
+      return(
+        <Home />
+      );
+    }else{
+      return (
+        <Login />
+      );
+    }
   }
 }
 
 export default App;
+
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import UserProfile from '../UserProfile.js';
+import '../App.css';
+
+
+class Home extends Component {
+  render() {
+    return(<h1>Home</h1>);
+  }
+}
+
+export default Home;
