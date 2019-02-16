a0ccf936a0673eb4a468d0c8d895fae07747b3c8
ceb3745 <ceb3745@g.rit.edu>
2019-02-16 16:24:07 -0500

    fixed the login submit to redirect to home

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,22 +2,44 @@ import React, { Component } from 'react';
 import UserProfile from '../UserProfile.js';
 import '../App.css';
 import Open from '../components/Open.js';
+import Home from '../components/Home.js';
 
 class Login extends Component {
   constructor(){
     super();
     this.state = {
       goBack : false,
+      username : '',
+      password : '',
+      submitted : false
     }
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleFormSubmit(){
+    //check to see if user and pass are valid combo on server
+    this.setState({
+      submitted: true
+    });
   }
 
   render() {
     if(this.state.goBack === true){
       return(<Open />);
+    }else if(this.state.submitted === true){
+      return(<Home />);
     }else{
       return(
         <div className="main">
           <h1>Login</h1>
+          <form className="main" onSubmit={this.handleFormSubmit}>
+            <h3>Username</h3>
+            <input type="text" name="username" onChange={e => this.setState({username: e.target.value})}/>
+            <h3>Password</h3>
+            <input type="password" name="password" onChange={e => this.setState({password: e.target.value})}/>
+            <button type="submit">Submit</button>
+          </form>
           <button onClick={e => this.setState({goBack : true})}>Back</button>
         </div>
       );
