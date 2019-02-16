f89bdb9ee258219ee5e9297e10de92c6234cc422
ceb3745 <ceb3745@g.rit.edu>
2019-02-16 15:41:01 -0500

    updated and fixed signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,9 +3,28 @@ import UserProfile from '../UserProfile';
 
 
 class SignUp extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+
+
   render() {
     return(
-      <h1>SignUp</h1>
+      <div className="main">
+        <h1>SignUp</h1>
+        <p>
+
+        </p>
+        <input type="text" name="email" onChange={e => this.setState({email: e.target.value })}/>
+        <input type="text" name="password" onChange={e => this.setState({email: e.target.value })}/>
+      </div>
+
     );
   }
 }
