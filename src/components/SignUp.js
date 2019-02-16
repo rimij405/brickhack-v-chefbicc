85f9a127bbbf68c5c56dfb97639724050781f753
ceb3745 <ceb3745@g.rit.edu>
2019-02-16 15:29:29 -0500

    created signup.js

---
+++ src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import UserProfile from '../UserProfile';
+
+
+class SignUp extends Component {
+  render() {
+    return(
+      <h1>SignUp</h1>
+    );
+  }
+}
+
+export default SignUp;
