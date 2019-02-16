728afd375cc722c20d795943be1d661c1d842dfd
Henry <hmf4455@rit.edu>
2019-02-16 17:18:35 -0500

    Add mongoose connection

--- src/mongodb-test.js
+++ src/mongodb-test.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+
 // Check port and debug mode values.
 const environment = process.env.NODE_ENV.trim() || 'dev';
 
@@ -8,6 +9,34 @@ const flags = {
     DEBUG: (environment === "dev"),
     PORT: (process.env.PORT || 3001)
 };
+
+
+const mongoose = require('mongoose');
+
+// In dev mode, connect to the test DB
+if (DEBUG) {
+    const db = mongoose.connect('mongodb://localhost/test');
+} 
+else {
+    // Undefined!
+    // const db = mongoose.connect('mongodb://localhost/OFFICIALDB
+}
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    moodIDs: [{ id: Number }]
+});
+
+// TODO determine representation each of these- what is a "mood"?
+const moodSchema = new mongoose.Schema({
+    uuid: Number,
+    date: String,
+    mood: { unicode: String }
+});
+
+
 
 app.get('/', (req, res) => {
     let resBody = "Hello mongodb!";
@@ -19,6 +48,10 @@ app.get('/', (req, res) => {
 
 app.get('/users/:userId', (req, res) => {
     res.send(req.params);
+});
+
+app.post('/mkuser', (req, res) => {
+
 });
 
 app.use('/time', (req, res, next) => {
