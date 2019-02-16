e133513ac21394264679ce09bdc42bc9e91f7812
clairechen <cxc2230@rit.edu>
2019-02-16 15:42:16 -0500

    added buttons to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,26 @@ import UserProfile from '../UserProfile';
 
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state={
+      switch: ''
+    }
+  }
+
   render() {
     return(
+      <div className="main">
       <h1>Login</h1>
+      <button onClick={e => this.setState({switch:'login'})}>
+        Login
+      </button>
+      <button onClick={e => this.setState({switch:'signup'})}>
+        Sign Up
+      </button>
+
+      </div>
+    
     );
   }
 }
