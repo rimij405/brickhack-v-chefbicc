92abd50be14744a220e4f371dc110f2cae073835
clairechen <cxc2230@rit.edu>
2019-02-16 17:05:46 -0500

    username validation

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -23,8 +23,10 @@ class SignUp extends Component {
   handlePassChange(value){
     if(this.state.password !== value){
       document.getElementById('passError').style.display = 'block';
+      return false;
     }else{
       document.getElementById('passError').style.display = 'none';
+      return true;
     }
   }
 
@@ -33,16 +35,33 @@ class SignUp extends Component {
     if(valid.test(value)) {
       this.setState({email: value});
       document.getElementById('emailError').style.display = 'none';
+      return true;
     }
     else {
       document.getElementById('emailError').style.display = 'block';
+      return false;
     }
   }
   handleSubmit(formSubmit){
     UserProfile.setName(this.state.email);
-    this.setState({
-      submitted: true
-    })
+    if (this.handleEmail(this.state.email) && this.handleUsername(this.state.username) && this.handlePassChange(this.state.password)) {
+      this.setState({
+        submitted: true
+      })
+    }
+
+ }
+
+ handleUsername(value) {
+   if (value.length === 0){
+     document.getElementById('usernameError').style.display = 'block';
+     return false;
+   }
+   else {
+     document.getElementById('usernameError').style.display = 'none';
+     this.setState({username: value})
+     return true;
+   }
  }
 
   render() {
@@ -56,18 +75,19 @@ class SignUp extends Component {
     else{
       return(
         <div className="main">
-          <form onSubmit={this.handleSubmit} className="main">
             <h1>SignUp</h1>
             <h3>Email Address</h3>
             <div className="error" id="emailError">Please enter a valid email</div>
             <input type="text" name="email" onChange={e => this.handleEmail(e.target.value)}/>
+            <h3>Username</h3>
+            <div className="error" id="usernameError">Please enter a username</div>
+            <input type="text" name="username" onChange={e =>this.handleUsername(e.target.value)}/>
             <h3>Password</h3>
             <input type="password" name="password" onChange={e => this.setState({password: e.target.value })}/>
             <h3>Re-Enter Password</h3>
             <div className="error" id="passError">Passwords Not the Same</div>
             <input type="password" name="password" onChange={e => this.handlePassChange(e.target.value)} />
-            <button type="submit">Sign Up</button>
-          </form>
+            <button onClick={e => this.handleSubmit()}>Sign Up</button>
           <button onClick={e => this.setState({goBack: true})}>Back</button>
           
         </div>
