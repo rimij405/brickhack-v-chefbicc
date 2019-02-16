274d3b7ca6d546ae209183be49306e758c2f38a0
Ian Effendi <effendiian@gmail.com>
2019-02-16 17:52:35 -0500

    Add models.

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+/*
+    User.js
+    Mongoose model for a User.
+*/
+
+
+/*
+// const crypto = require('crypto');
+const mongoose = require('mongoose');
+
+// Set promise.
+mongoose.Promise = global.Promise;
+
+// Create the user model.
+const UserModel = {};
+const iterations = 10000;
+const saltLength = 64;
+const keyLength = 64;
+
+// Create the model schema.
+const UserSchema = new mongoose.Schema({
+
+});
+
+// Static methods.
+UserSchema.statics.toAPI = (doc) => {
+
+};
+*/
--- src/server.js
+++ src/server.js
@@ -1,27 +1,28 @@
 const express = require('express');
+
 const app = express();
 
 // Check port and debug mode values.
 const flags = {
-    DEBUG: (process.env.NODE_ENV.trim() === "dev"),
-    PORT: (process.env.PORT || 3001)
+  DEBUG: (process.env.NODE_ENV.trim() === 'dev'),
+  PORT: (process.env.PORT || 3001),
 };
 
 app.get('/', (req, res) => {
-    let resBody = "Hello world!";
-    if(flags.DEBUG) {
-        resBody += " Debug mode.";        
-    }
-    res.send(resBody);    
+  let resBody = 'Hello world!';
+  if (flags.DEBUG) {
+    resBody += ' Debug mode.';
+  }
+  res.send(resBody);
 });
 
 app.get('/users/:userId', (req, res) => {
-    res.send(req.params);
+  res.send(req.params);
 });
 
 app.use('/time', (req, res, next) => {
-    console.log('Time: ', Date.now());
-    next();
+  console.log('Time: ', Date.now());
+  next();
 });
 
 app.listen(flags.PORT, () => console.log(`Server listening on port ${flags.PORT}!`));
