4daa727bbf6c6085ad049606ceada8584cffd462
Ian Effendi <effendiian@gmail.com>
2019-02-16 19:29:36 -0500

    Add router.

---
+++ src/router.js
@@ -0,0 +1,11 @@
+// Using statements.
+// const controllers = require('./controllers');
+// const middleware = require('./middleware');
+
+// Router to export.
+const router = (app) => {
+    
+};
+
+// Export the router.
+module.exports = router;
